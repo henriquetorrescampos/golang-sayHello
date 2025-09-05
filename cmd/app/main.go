beae3da5ee53397bac3b9022ab2bfc5d9eae7d72
henriquetorrescampos <henriquetorrescampos@gmail.com>
2025-09-05 20:21:45 -0300

    adding fibonnaci function

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,14 +2,30 @@
 // Convenção de mercado: colocar em cmd/<nome-app>/main.go
 package main
 
-// Importa os pacotes necessários 
+// Importa os pacotes necessários
 import (
-    "fmt"
-    "github.com/seu-usuario/meu-projeto-go/internal/hello"
+	"fmt"
+
+	fibonnaci "github.com/seu-usuario/meu-projeto-go/internal/fibonacci"
+	"github.com/seu-usuario/meu-projeto-go/internal/hello"
 )
 
 // Função principal do programa
 func main() {
-    fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
-    hello.SayHello()
+	fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
+	hello.SayHello()
+
+	var num int
+
+	fmt.Print("Digita um número para calcular fibonnaci: ")
+	_, err := fmt.Scan(&num)
+
+	if err != nil {
+		fmt.Println("Error ao ler número.")
+		return
+	}
+
+	fibResult, _ := fibonnaci.Fibonnaci(num)
+
+	fmt.Println("O reusltado Fibonnaci do número", num, "é", fibResult)
 }
